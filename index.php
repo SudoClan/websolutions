1cbd35b6eee27a8bc9dd55994af1d377f7167d7f
saifdevops <saifulmalook1@hotmail.com>
2017-12-19 01:04:49 +0000

    removed html page and replace with index php to speed up production

--- index.php
+++ index.php
@@ -1,1 +1,361 @@
+<?php include "includes/header.php" ?>
+<?php include "includes/nav.php" ?>
 <?php include "includes/bannerHome.php" ?>
+
+<!-- Services Area Start -->
+<div id="services">
+    <div class="container">
+        <!-- Section Title Start -->
+        <div class="section--title">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>Our <span>Services</span></h2>
+                </div>
+                <div class="col-md-6">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus iste dignissimos quidem quod, ex omnis alias enim quae.</p>
+                </div>
+            </div>
+        </div>
+        <!-- Section Title End -->
+        <div class="row">
+            <div class="col-md-4 service--item-h">
+                <!-- Service Item Start -->
+                <div class="service--item">
+                    <div class="service--icon">
+                        <img src="img/service-img/icon-01.png" alt="">
+                    </div>
+                    <div class="service--content">
+                        <h3><span>Shared</span> Hosting</h3>
+                        <p>Lorem ipsum dolor sit consectetur adipisicing elit ducimus pariatur.</p>
+                    </div>
+                </div>
+                <!-- Service Item End -->
+            </div>
+            <div class="col-md-4 service--item-h">
+                <!-- Service Item Start -->
+                <div class="service--item">
+                    <div class="service--icon">
+                        <img src="img/service-img/icon-02.png" alt="">
+                    </div>
+                    <div class="service--content">
+                        <h3><span>Cloud</span> Hosting</h3>
+                        <p>Lorem ipsum dolor sit consectetur adipisicing elit ducimus pariatur.</p>
+                    </div>
+                </div>
+                <!-- Service Item End -->
+            </div>
+            <div class="col-md-4 service--item-h">
+                <!-- Service Item Start -->
+                <div class="service--item">
+                    <div class="service--icon">
+                        <img src="img/service-img/icon-03.png" alt="">
+                    </div>
+                    <div class="service--content">
+                        <h3><span>Dedicated</span> Server</h3>
+                        <p>Lorem ipsum dolor sit consectetur adipisicing elit ducimus pariatur.</p>
+                    </div>
+                </div>
+                <!-- Service Item End -->
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Services Area End -->
+<!-- Domain Search Area Start -->
+<div id="domainSearch" class="bg--whitesmoke">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <!-- Section Title Start -->
+                <div class="section--title block">
+                    <h2>Looking A Premium Quality<span>Domain Name?</span></h2>
+                </div>
+                <!-- Section Title End -->
+                <!-- Domain Search Form Start -->
+                <div class="domain-search--form">
+                    <form action="http://billing.ywhmcs.com/domainchecker.php?systpl=HostWHMCScv1" method="post">
+                        <div class="input--text">
+                            <input type="text" name="domain" placeholder="eg. example" class="form-control">
+                            <span class="highlight"></span>
+                        </div>
+
+                        <div class="input--radio">
+                            <label class="radio-inline">
+                                <input type="radio" name="ext" value=".com" checked="checked">
+                                <span>.com</span>
+                            </label>
+                            <label class="radio-inline">
+                                <input type="radio" name="ext" value=".net">
+                                <span>.net</span>
+                            </label>
+                            <label class="radio-inline">
+                                <input type="radio" name="ext" value=".org">
+                                <span>.org</span>
+                            </label>
+                            <label class="radio-inline">
+                                <input type="radio" name="ext" value=".info">
+                                <span>.info</span>
+                            </label>
+                            <label class="radio-inline">
+                                <input type="radio" name="ext" value=".biz">
+                                <span>.biz</span>
+                            </label>
+                            <label class="radio-inline">
+                                <input type="radio" name="ext" value=".us">
+                                <span>.us</span>
+                            </label>
+                        </div>
+
+                        <button type="submit" class="btn--primary btn--ripple">Search</button>
+                    </form>
+                </div>
+                <!-- Domain Search Form End -->
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Domain Search Area End -->
+<!-- Pricing Area Start -->
+<div id="pricing">
+    <div class="container">
+        <!-- Section Title Start -->
+        <div class="section--title">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>Our <span>Pricing</span></h2>
+                </div>
+                <div class="col-md-6">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus iste dignissimos quidem quod, ex omnis alias enim quae.</p>
+                </div>
+            </div>
+        </div>
+        <!-- Section Title End -->
+        <div class="row reset-gutter">
+            <div class="col-md-4 pricing--item-h">
+                <!-- Pricing Item Start -->
+                <div class="pricing--item">
+                    <div class="pricing--header">
+                        <div class="pricing--icon">
+                            <img src="img/pricing-img/shared.png" alt="" class="img-responsive center-block">
+                        </div>
+                    </div>
+                    <div class="pricing--body">
+                        <div class="pricing--tag">
+                            <p>Starting At <strong>$9.99</strong></p>
+                        </div>
+                        <div class="pricing--details">
+                            <h2><strong>Shared</strong> Hosting</h2>
+                            <p>Lorem ipsum dolor sit consectetur amet, adipisicing elit molestias.</p>
+                        </div>
+                    </div>
+                    <div class="pricing--footer">
+                        <a href="#" class="btn--primary btn--ripple">Order Now</a>
+                    </div>
+                </div>
+                <!-- Pricing Item End -->
+            </div>
+            <div class="col-md-4 pricing--item-h">
+                <!-- Pricing Item Start -->
+                <div class="pricing--item raised">
+                    <div class="pricing--header">
+                        <div class="pricing--icon">
+                            <img src="img/pricing-img/cloud-vps.png" alt="" class="img-responsive center-block">
+                        </div>
+                    </div>
+                    <div class="pricing--body">
+                        <div class="pricing--tag">
+                            <p>Starting At <strong>$29.99</strong></p>
+                        </div>
+                        <div class="pricing--details">
+                            <h2><strong>Cloud</strong> Hosting</h2>
+                            <p>Lorem ipsum dolor sit consectetur amet, adipisicing elit molestias.</p>
+                        </div>
+                    </div>
+                    <div class="pricing--footer">
+                        <a href="#" class="btn--primary btn--ripple">Order Now</a>
+                    </div>
+                </div>
+                <!-- Pricing Item End -->
+            </div>
+            <div class="col-md-4 pricing--item-h">
+                <!-- Pricing Item Start -->
+                <div class="pricing--item">
+                    <div class="pricing--header">
+                        <div class="pricing--icon">
+                            <img src="img/pricing-img/dedicated.png" alt="" class="img-responsive center-block">
+                        </div>
+                    </div>
+                    <div class="pricing--body">
+                        <div class="pricing--tag">
+                            <p>Starting At <strong>$49.99</strong></p>
+                        </div>
+                        <div class="pricing--details">
+                            <h2><strong>Dedicated</strong> Hosting</h2>
+                            <p>Lorem ipsum dolor sit consectetur amet, adipisicing elit molestias.</p>
+                        </div>
+                    </div>
+                    <div class="pricing--footer">
+                        <a href="#" class="btn--primary btn--ripple">Order Now</a>
+                    </div>
+                </div>
+                <!-- Pricing Item End -->
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Pricing Area End -->
+<!-- Feedback Area Start -->
+<div id="feedback" class="bg--whitesmoke">
+    <div class="container">
+        <!-- Section Title Start -->
+        <div class="section--title">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>Clients <span>Feedback</span></h2>
+                </div>
+                <div class="col-md-6">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus iste dignissimos quidem quod, ex omnis alias enim quae.</p>
+                </div>
+            </div>
+        </div>
+        <!-- Section Title End -->
+        <div class="feedback--nav-tabs">
+            <ul class="nav nav-tabs">
+                <li>
+                    <a href="#feedbackTab1" data-toggle="tab">
+                        <img src="img/feedback-img/icon-01.png" alt="" class="img-responsive">
+                    </a>
+                </li>
+                <li class="active">
+                    <a href="#feedbackTab2" data-toggle="tab">
+                        <img src="img/feedback-img/icon-02.png" alt="" class="img-responsive">
+                    </a>
+                </li>
+                <li>
+                    <a href="#feedbackTab3" data-toggle="tab">
+                        <img src="img/feedback-img/icon-03.png" alt="" class="img-responsive">
+                    </a>
+                </li>
+                <li>
+                    <a href="#feedbackTab4" data-toggle="tab">
+                        <img src="img/feedback-img/icon-04.png" alt="" class="img-responsive">
+                    </a>
+                </li>
+            </ul>
+            <div class="feedback--triangle"></div>
+        </div>
+
+        <div class="feedback--tabs">
+            <div class="tab-content">
+                <div class="tab-pane fade" id="feedbackTab1">
+                    <blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit vitae ab id natus, saepe molestiae nisi aliquid autem blanditiis! Totam laudantium necessitatibus aspernatur, facilis quaerat voluptatibus nobis perspiciatis error nisi!</blockquote>
+                </div>
+                <div class="tab-pane fade in active" id="feedbackTab2">
+                    <blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit vitae ab id natus, saepe molestiae nisi aliquid autem blanditiis! Totam laudantium necessitatibus aspernatur, facilis quaerat voluptatibus nobis perspiciatis error nisi!</blockquote>
+                </div>
+                <div class="tab-pane fade" id="feedbackTab3">
+                    <blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit vitae ab id natus, saepe molestiae nisi aliquid autem blanditiis! Totam laudantium necessitatibus aspernatur, facilis quaerat voluptatibus nobis perspiciatis error nisi!</blockquote>
+                </div>
+                <div class="tab-pane fade" id="feedbackTab4">
+                    <blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit vitae ab id natus, saepe molestiae nisi aliquid autem blanditiis! Totam laudantium necessitatibus aspernatur, facilis quaerat voluptatibus nobis perspiciatis error nisi!</blockquote>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Feedback Area End -->
+<!-- Features Area Start -->
+<div id="features">
+    <div class="container">
+        <!-- Section Title Start -->
+        <div class="section--title">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>Services <span>Included</span></h2>
+                </div>
+                <div class="col-md-6">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus iste dignissimos quidem quod, ex omnis alias enim quae.</p>
+                </div>
+            </div>
+        </div>
+        <!-- Section Title End -->
+        <div class="row">
+            <div class="col-md-4 feature--item-h">
+                <!-- Feature Item Start -->
+                <div class="feature--item">
+                    <div class="feature--icon">
+                        <img src="img/feature-img/icon-01.png" alt="" class="img-responsive center-block">
+                    </div>
+                    <div class="feature--content">
+                        <h2><span>24/7/365</span> Support</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo libero debitis quos dolorum adipisci architecto provident quaerat.</p>
+                    </div>
+                </div>
+                <!-- Feature Item End -->
+            </div>
+            <div class="col-md-4 feature--item-h">
+                <!-- Feature Item Start -->
+                <div class="feature--item">
+                    <div class="feature--icon">
+                        <img src="img/feature-img/icon-02.png" alt="" class="img-responsive center-block">
+                    </div>
+                    <div class="feature--content">
+                        <h2><span>45 Day</span> Guarantee</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo libero debitis quos dolorum adipisci architecto provident quaerat.</p>
+                    </div>
+                </div>
+                <!-- Feature Item End -->
+            </div>
+            <div class="col-md-4 feature--item-h">
+                <!-- Feature Item Start -->
+                <div class="feature--item">
+                    <div class="feature--icon">
+                        <img src="img/feature-img/icon-03.png" alt="" class="img-responsive center-block">
+                    </div>
+                    <div class="feature--content">
+                        <h2><span>99.9%</span> Uptime</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo libero debitis quos dolorum adipisci architecto provident quaerat.</p>
+                    </div>
+                </div>
+                <!-- Feature Item End -->
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Features Area End -->
+<!-- Subscribe Area Start -->
+<div id="subscribe">
+    <div class="subscribe--sticky">
+        <div class="container">
+            <!-- Subscribe Content Start -->
+            <div class="subscribe--content" data-bg-img="img/subscribe-img/bg.png">
+                <div class="row">
+                    <div class="col-md-4">
+                        <!-- Section Title Start -->
+                        <div class="section--title block">
+                            <h2>Subscribe To Our<span>Newsletter</span></h2>
+                        </div>
+                        <!-- Section Title End -->
+                    </div>
+                    <div class="col-md-8">
+                        <!-- Subscribe Form Start -->
+                        <div class="subscribe--form">
+                            <form action="http://themelooks.us12.list-manage.com/subscribe/post?u=50e1e21235cbd751ab4c1ebaa&amp;id=ac81d988e4" method="post" name="mc-embedded-subscribe-form" target="_blank" novalidate="novalidate">
+                                <div class="input--text">
+                                    <input type="text" value="" name="EMAIL" placeholder="Enter your email address">
+                                    <span class="highlight"></span>
+                                </div>
+                                <button type="submit" class="btn--primary btn--ripple">Subscribe</button>
+                            </form>
+                        </div>
+                        <!-- Subscribe Form End -->
+                    </div>
+                </div>
+            </div>
+            <!-- Subscribe Content End -->
+        </div>
+    </div>
+</div>
+<!-- Subscribe Area End -->
+
+<?php include "includes/footer.php" ?>
