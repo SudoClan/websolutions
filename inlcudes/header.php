26593f18849a589a7a6f70184210164836b9105a
saifdevops <saifulmalook1@hotmail.com>
2017-12-19 00:55:36 +0000

    added header php include

---
+++ inlcudes/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>HostWHMCS - Responsive Web Hosting and WHMCS Template</title>
+
+    <!-- ====Favicons==== -->
+    <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
+    <link rel="icon" href="favicon.png" type="image/x-icon">
+
+
+    <!-- ====Google Font Stylesheet==== -->
+    <link href='https://fonts.googleapis.com/css?family=Raleway:300,400,900' rel='stylesheet' type='text/css'>
+
+    <!-- ====Font Awesome Stylesheet==== -->
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- ====Bootstrap Stylesheet==== -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- ====bxSlider Plugin==== -->
+    <link href="css/jquery.bxslider.min.css" rel="stylesheet">
+
+    <!-- ====Main Stylesheet==== -->
+    <link href="style.css" rel="stylesheet">
+
+    <!-- ====Responsive Stylesheet==== -->
+    <link href="css/responsive-style.css" rel="stylesheet">
+
+    <!-- ====Theme Color Stylesheet==== -->
+    <link href="css/colors/theme-color-1.css" rel="stylesheet" id="changeColorScheme">
+
+    <!-- HTML5 Shim and Respond.js IE9 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
