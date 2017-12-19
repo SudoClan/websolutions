a426f7278597d599175ccf80f7d57520179441cc
saifdevops <saifulmalook1@hotmail.com>
2017-12-19 23:43:39 +0000

    added the code, a sample from bannerHome.php ready to be customised

---
+++ includes/bannerContact.php
@@ -0,0 +1,94 @@
+<!-- Banner Area Start -->
+<div id="banner">
+    <div class="banner--bg-animate" data-bg-img="img/banner-slider-img/rocket.png"></div>
+    <div class="vc-parent">
+        <div class="vc-child">
+            <!-- Banner Slider Start -->
+            <div class="banner--slider BannerSlider">
+                <!-- Banner Item Start -->
+                <div class="banner--item">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <!-- Banner Content Start -->
+                                <div class="banner--content">
+                                    <h3>Shared</h3>
+                                    <h2>Hosting</h2>
+
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At numquam laboriosam sapiente vitae repellendus itaque mollitia, quis autem! Minima debitis nemo omnis, neque accusantium amet maxime suscipit.</p>
+
+                                    <ul>
+                                        <li>Lorem ipsum dolor sit amet.</li>
+                                        <li>Lorem ipsum dolor sit amet.</li>
+                                        <li>Lorem ipsum dolor sit amet.</li>
+                                        <li>Lorem ipsum dolor sit amet.</li>
+                                    </ul>
+
+                                    <a href="#" class="btn--primary btn--ripple">Learn More</a>
+                                </div>
+                                <!-- Banner Content End -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Banner Item End -->
+                <!-- Banner Item Start -->
+                <div class="banner--item">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <!-- Banner Content Start -->
+                                <div class="banner--content">
+                                    <h3>VPS</h3>
+                                    <h2>Hosting</h2>
+
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At numquam laboriosam sapiente vitae repellendus itaque mollitia, quis autem! Minima debitis nemo omnis, neque accusantium amet maxime suscipit.</p>
+
+                                    <ul>
+                                        <li>Lorem ipsum dolor sit amet.</li>
+                                        <li>Lorem ipsum dolor sit amet.</li>
+                                        <li>Lorem ipsum dolor sit amet.</li>
+                                        <li>Lorem ipsum dolor sit amet.</li>
+                                    </ul>
+
+                                    <a href="#" class="btn--primary btn--ripple">Learn More</a>
+                                </div>
+                                <!-- Banner Content End -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Banner Item End -->
+                <!-- Banner Item Start -->
+                <div class="banner--item">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <!-- Banner Content Start -->
+                                <div class="banner--content">
+                                    <h3>Dedicated</h3>
+                                    <h2>Server</h2>
+
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At numquam laboriosam sapiente vitae repellendus itaque mollitia, quis autem! Minima debitis nemo omnis, neque accusantium amet maxime suscipit.</p>
+
+                                    <ul>
+                                        <li>Lorem ipsum dolor sit amet.</li>
+                                        <li>Lorem ipsum dolor sit amet.</li>
+                                        <li>Lorem ipsum dolor sit amet.</li>
+                                        <li>Lorem ipsum dolor sit amet.</li>
+                                    </ul>
+
+                                    <a href="#" class="btn--primary btn--ripple">Learn More</a>
+                                </div>
+                                <!-- Banner Content End -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Banner Item End -->
+            </div>
+            <!-- Banner Slider End -->
+        </div>
+    </div>
+</div>
+<!-- Banner Area End -->
