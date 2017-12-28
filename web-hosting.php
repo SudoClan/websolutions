bf4eaf31701d460dba85e5bdaa930693086810e4
Umar <umark.ws@gmail.com>
2017-12-28 23:09:40 +0000

    Added h3 title tag with custom css

--- web-hosting.php
+++ web-hosting.php
@@ -3,7 +3,10 @@
 <?php include "includes/bannerWebHosting.php" ?>
 <div class="container">
 
-
+<br />
+<h3>No Limits. No Contracts. No Hassle.<br />
+UK Unlimited SSD Cloud Web Hosting with cPanel</h3>
+<br />
 
 </div>
 <?php include "includes/footer.php" ?>
