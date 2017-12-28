401603a012e879601f4c8fbcb7356050648b6171
Umar <umark.ws@gmail.com>
2017-12-28 23:43:28 +0000

    Added carousel text only for reviews under h3 tag with custom css.

--- web-hosting.php
+++ web-hosting.php
@@ -6,7 +6,51 @@
 <br />
 <h3>No Limits. No Contracts. No Hassle.<br />
 UK Unlimited SSD Cloud Web Hosting with cPanel</h3>
-<br />
+<hr>
+
+<div id="text-carousel" class="carousel slide" data-ride="carousel">
+    <!-- Wrapper for slides -->
+    <div class="row">
+        <div class="col-xs-offset-0">
+            <div class="carousel-inner">
+                <div class="item active">
+                    <div class="carousel-content">
+                        <div>
+                          <h3>“Great service, excellent comunication”<br /><br />
+                          <img src="img/star.png"/> <img src="img/star.png"/> <img src="img/star.png"/> <img src="img/star.png"/> <img src="img/star.png"/></h3>
+                          <br />
+                          <p>I was looking for someone to make me a website after the previous person who made one for me let me down. Web solution sent me a short & sweet message on facebook that showed me straight away this is the right company for me to choose...</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="carousel-content">
+                      <div>
+                        <h3>“Amazing Web Development and Hosting Company”<br /><br />
+                        <img src="img/star.png"/> <img src="img/star.png"/> <img src="img/star.png"/> <img src="img/star.png"/> <img src="img/star.png"/></h3>
+                        <br />
+                        <p>I've worked with Web Solutions (London) Limited for two years now, I'm very satisfied with their service. They are fully dedicated to their customers and there is no way that I could not refer them. Well Recommended.</p>
+                      </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="carousel-content">
+                      <div>
+                        <h3>“These guys make better webs than spiderman!”<br /><br />
+                        <img src="img/star.png"/> <img src="img/star.png"/> <img src="img/star.png"/> <img src="img/star.png"/> <img src="img/star.png"/></h3>
+                        <br />
+                        <p>I have been using Web Solutions Company for about 3 years now & they are currently in the middle of constructing my 3rd website for my 3rd company. Theres a reason why i keep going back to them despite getting calls, leaflets & emails...</p>
+                      </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</div>
+<div class="carousel-button"><a href="#" class="btn--primary btn--ripple">Read More</a></div>
+
 
 </div>
 <?php include "includes/footer.php" ?>
