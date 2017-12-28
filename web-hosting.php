fca0d1887cea22601ed9f9e7899362eff92cc5bb
Umar <umark.ws@gmail.com>
2017-12-28 22:42:46 +0000

    Added container div

--- web-hosting.php
+++ web-hosting.php
@@ -1,7 +1,9 @@
 <?php include "includes/header.php" ?>
 <?php include "includes/nav.php" ?>
 <?php include "includes/bannerWebHosting.php" ?>
+<div class="container">
 
 
 
+</div>
 <?php include "includes/footer.php" ?>
