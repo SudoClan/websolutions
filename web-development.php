4b327ab528d1a6ae591d242d974fb61d7f8bb9c4
saifdevops <saifulmalook1@hotmail.com>
2018-01-02 20:04:51 +0000

    added a section to show basic info about packages

--- web-development.php
+++ web-development.php
@@ -2,7 +2,42 @@
 <?php include "includes/nav.php" ?>
 <?php include "includes/bannerWebDevelopment.php" ?>
 <?php include "includes/reviews.php" ?>
+<!-- Pricing Area 2 Start -->
+        <div id="pricing2">
+            <div class="container">
+                <div class="row">
+                    <!-- Pricing 2 Item Start -->
+                    <div class="col-md-4 pricing-2--item">
+                        <div class="pricing-2--header">
+                            <h3 class="pricing-2--title">1 - 14 Monthly Sales</h3>
+                            <h4 class="pricing-2--price">Per Referral <span>$9.99</span></h4>
+                        </div>
+                    </div>
+                    <!-- Pricing 2 Item End -->
+                    <!-- Pricing 2 Item Start -->
+                    <div class="col-md-4 pricing-2--item">
+                        <div class="pricing-2--header">
+                            <h3 class="pricing-2--title">15 - 29 Monthly Sales</h3>
+                            <h4 class="pricing-2--price">Per Referral <span>$19.99</span></h4>
+                        </div>
+                    </div>
+                    <!-- Pricing 2 Item End -->
+                    <!-- Pricing 2 Item Start -->
+                    <div class="col-md-4 pricing-2--item">
+                        <div class="pricing-2--header">
+                            <h3 class="pricing-2--title">30 - 69 Monthly Sales</h3>
+                            <h4 class="pricing-2--price">Per Referral <span>$29.99</span></h4>
+                        </div>
+                    </div>
+                    <!-- Pricing 2 Item End -->
+                </div>
 
+                <div class="section--footer">
+                    <a href="#" class="btn--primary btn--ripple">Signup Now</a>
+                </div>
+            </div>
+        </div>
+        <!-- Pricing Area 2 End -->
 
 
 <?php include "includes/footer.php" ?>
