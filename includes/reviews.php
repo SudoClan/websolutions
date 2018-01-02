cdc4010db76a316bd66c9203a445b6d09ccc64fa
saifdevops <saifulmalook1@hotmail.com>
2018-01-02 19:58:37 +0000

    new include file called reviews, have attached to webdev page-working

---
+++ includes/reviews.php
@@ -0,0 +1,35 @@
+<!-- Reviews Section START -->
+<div class="container">
+
+<br />
+<h3 class="reviewTitle">No Limits. No Contracts. No Hassle.<br />
+UK Unlimited SSD Cloud Web Hosting with cPanel</h3>
+<hr>
+
+<div style="margin:auto;">
+<div class="col-md-3">
+  <h3 class="reviewHeading">Professional &amp; reputable hosting from a company you can trust</h3>
+  <a href="#" class="btn--primary btn--ripple">Read More</a>
+</div>
+
+<div class="col-md-3">
+  <img src="img/star.png" /> <img src="img/star.png" /> <img src="img/star.png" /> <img src="img/star.png" /> <img src="img/star.png" /><br />
+  <h3>Professional streamline service</h3>
+  <p>I got a few changes done to my website. Which were fantastic as they advised me on a few things i didn't think about. All within reasonable price . Their upsell of business cards were next to none with quality and great rate.</p>
+</div>
+
+<div class="col-md-3">
+  <img src="img/star.png" /> <img src="img/star.png" /> <img src="img/star.png" /> <img src="img/star.png" /> <img src="img/star.png" />
+  <h3>Great service, excellent comunication</h3>
+  <p>I was looking for someone to make me a website after the previous person who made one for me let me down. Web solution sent me a short & sweet message on facebook that showed me straight away this is the right company...</p>
+</div>
+
+<div class="col-md-3">
+  <img src="img/star.png" /> <img src="img/star.png" /> <img src="img/star.png" /> <img src="img/star.png" /> <img src="img/star.png" />
+  <h3>These guys make better webs than spiderman!</h3>
+  <p>I have been using Web Solutions Company for about 3 years now & they are currently in the middle of constructing my 3rd website for my 3rd company. Theres a reason why i keep going back to them despite getting calls...</p>
+</div>
+</div>
+</div>
+<br>
+<!-- Reviews Section END -->
--- web-development.php
+++ web-development.php
@@ -1,6 +1,7 @@
 <?php include "includes/header.php" ?>
 <?php include "includes/nav.php" ?>
 <?php include "includes/bannerWebDevelopment.php" ?>
+<?php include "includes/reviews.php" ?>
 
 
 
