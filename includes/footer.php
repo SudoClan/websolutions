5367e7556362ed1050280470dc422f998293feb5
Umar <umark.ws@gmail.com>
2018-09-09 12:11:57 +0100

    Banner edited

--- includes/footer.php
+++ includes/footer.php
@@ -15,7 +15,7 @@ var JFL_82501696084359 = new JotformFeedback({
 <div class="" style="background:#e82121;">
 <div class="">
     <div style="margin:auto; text-align:center;">
-<a class="btn--primary btn--ripple btn lightbox-82501696084359" style="margin:30px; font-size:30px;">
+<a class="btn--primary btn--ripple btn lightbox-82501696084359 size-quote">
 Click Here for a Free Quote
 </a>
   </div>
