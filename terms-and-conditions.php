6b830250eec31c0dc1607a7207096af734e8444f
Umar <umark.ws@gmail.com>
2018-09-09 20:47:30 +0100

    Content updated

--- terms-and-conditions.php
+++ terms-and-conditions.php
@@ -2,6 +2,43 @@
 <?php include "includes/nav.php" ?>
 <?php include "includes/bannerTerms-and-Conditions.php" ?>
 
+<br /><br /><br />
+<div class="container">
+This page sets out the terms and conditions on which we supply any of our services listed on our website, www.websolutions.services ("our site") to you. Please read these terms and conditions carefully before ordering any of our services. You understand that by ordering any of our services, you agree to be bound by these terms and conditions.
+<br /><br />
+Should you wish to print a copy of these terms and conditions for future reference, press ctrl + p to do so.
 
+<h3>Information About Us</h3>
+We are registered in England and Wales under company number 09268011 and our registered office is 131 Gordon Road, Ilford, Essex, IG1 2XT, trading at 276 Kingston Road, Ilford, Essex, IG1 1PQ.
+
+<h3>Website Projects</h3>
+All our clients’ information is taken confidentiality. Once payment has been agreed, a 50% deposit will be required via Bank transfer before going ahead.
+
+After deposit has been made, your project will be secured and we will keep you up-to-date with the progress. We aim to complete your website ensuring we have all the required information. Once you are happy with; the build, design and functionality, final payment is required before your product is released.
+
+<h3>Hosting</h3>
+If you are hosting with us, an Online Secure Payment System (SSL) will be used, to take yearly or monthly payments for a Domain & Hosting Package. We accept all types of credit and debit cards. We may ask you to transfer the yearly payment via bank transfer or yearly/monthly payments via direct debit.
+
+<h3>Future costs</h3>
+Any updates in the future are subject to a further charge, (you do get some revisions as part of your package).
+
+<h3>Designing</h3>
+100% deposit will be required via Bank transfer before going ahead. We will proceed with your artwork with few revisions. Once your satisfied with the design, we will provide you high definition quality with the format PDF & JPG.
+
+<h3>Printing</h3>
+The first procedure is to verify the artwork, quality of the artwork and the bleeds of the artwork before printing. You as a user will need to verify all the information on the design as we are not liable for any error during the printing. Once printing has initiated, cancellation will not be possible.
+
+<h3>Delivery</h3>
+Please allow 3 – 5 days for your product to be delivered to your business address, unless specified. Once delivery process has initiated, cancellation will not be possible.
+
+<h3>Termination</h3>
+If you decide to terminate the project, depending on the work that has been carried out, you may/may not be able to receive a full or part refund of your deposit. Please ensure you let us know in advance to avoid disappointment.
+
+<h3>Contacting Us</h3>
+If you’d like to get in touch regarding any concerns to our Terms and Condition, please get in touch via the website contact form or info@websolutions.services.
+<br /><br />
+Please note, our terms and conditions are subject to change without notice.
+</div>
+<br /><br /><br />
 
 <?php include "includes/footer.php" ?>
