c26d066a05e7d8b0822ee844298d3da211c1034d
Umar <umark.ws@gmail.com>
2018-09-09 20:49:15 +0100

    Content updated

--- privacy-policy.php
+++ privacy-policy.php
@@ -2,6 +2,160 @@
 <?php include "includes/nav.php" ?>
 <?php include "includes/bannerPrivacy-Policy.php" ?>
 
+<br /><br /><br />
+<div class="container">
+Websolutions (London) Limited understands that your privacy is important to you and that you care about how your personal data is used. We respect and value the privacy of everyone who visits this website, https://websolutions.services (“Our Site”) and will only collect and use personal data in ways that are described here, and in a way that is consistent with our obligations and your rights under the law.
+<br /><br />
+Please read this Privacy Policy carefully and ensure that you understand it. Your acceptance of this Privacy Policy is deemed to occur upon your first use of Our Site. If you do not accept and agree with this Privacy Policy, you must stop using Our Site immediately.
 
+<ol>
+  <li>
+    <h3>Information About Us</h3>
+    Our Site is owned and operated by Websolutions (London) Limited, a Limited Company registered in England under company number 09268011.<br />
+    <b>Registered address:</b> 131 Gordon Road, Ilford, Essex, IG1 2XT.<br />
+    <b>Main trading address:</b> 276 Kingston Road, Ilford, Essex, IG1 1PQ.<br />
+    <b>Data Protection Officer:</b> Umar Kaleem.<br />
+    <b>Email address:</b> GDPR@websolutions.services.<br />
+    <b>Telephone number:</b> 0208 064 0276.<br />
+    <b>Postal Address:</b>  276 Kingston Road, Ilford, Essex, IG1 1PQ.
+
+  </li>
+  <li>
+    <h3>What Does This Policy Cover?</h3>
+    This Privacy Policy applies only to your use of Our Site. Our Site may contain links to other websites. Please note that we have no control over how your data is collected, stored, or used by other websites and we advise you to check the privacy policies of any such websites before providing any data to them.
+  </li>
+  <li>
+    <h3>What is Personal Data?</h3>
+    Personal data is defined by the General Data Protection Regulation (EU Regulation 2016/679) (the “GDPR”) as ‘any information relating to an identifiable person who can be directly or indirectly identified in particular by reference to an identifier’.
+    <br /><br />
+    Personal data is, in simpler terms, any information about you that enables you to be identified. Personal data covers obvious information such as your name and contact details, but it also covers less obvious information such as identification numbers, electronic location data, and other online identifiers.
+  </li>
+  <li>
+    <h3>What Are My Rights?</h3>
+    Under the GDPR, you have the following rights, which we will always work to uphold:
+    <ol type="a">
+      <li>The right to be informed about our collection and use of your personal data. This Privacy Policy should tell you everything you need to know, but you can always contact us to find out more or to ask any questions using the details in Part 12.</li>
+      <li>The right to access the personal data we hold about you. Part 11 will tell you how to do this.</li>
+      <li>The right to have your personal data rectified if any of your personal data held by us is inaccurate or incomplete. Please contact us using the details in Part 12 to find out more.</li>
+      <li>The right to be forgotten, i.e. the right to ask us to delete or otherwise dispose of any of your personal data that we have. Please contact us using the details in Part 12 to find out more.</li>
+      <li>The right to restrict (i.e. prevent) the processing of your personal data.</li>
+      <li>The right to object to us using your personal data for a particular purpose or purposes.</li>
+      <li>The right to data portability. This means that, if you have provided personal data to us directly, we am using it with your consent or for the performance of a contract, and that data is processed using automated means, you can ask us for a copy of that personal data to re-use with another service or business in many cases.</li>
+      <li>Rights relating to automated decision-making and profiling. We do not use your personal data in this way.</li>
+    </ol>
+  </li>
+  <br />
+  For more information about our use of your personal data or exercising your rights as outlined above, please contact us using the details provided in Part 12.<br /><br />
+  Further information about your rights can also be obtained from the Information Commissioner’s Office or your local Citizens Advice Bureau.
+  If you have any cause for complaint about our use of your personal data, you have the right to lodge a complaint with the Information Commissioner’s Office.
+  <li>
+    <h3>What Data Do You Collect?</h3>
+    Personal data, or personal information, means any information about an individual from which that person can be identified.
+It does not include data where the identity has been removed (anonymous data).
+<br /><br />
+We may collect, use, store and transfer different kinds of personal data about you which we have grouped together as follows:
+<ol type="a">
+    <li>Identity Data includes first name, maiden name, last name, username or similar identifier, marital status, title, date of birth and gender.</li>
+    <li>Contact Data includes billing address, delivery address, email address and telephone numbers.</li>
+    <li>Financial Data includes bank account and payment card details.</li>
+    <li>Transaction Data includes details about payments to and from you and other details of products and services you have purchased from us.</li>
+    <li>Technical Data includes internet protocol (IP) address, your login data, browser type and version, time zone setting and location, browser plug-in types and versions, operating system and platform and other technology on the devices you use to access this website.</li>
+    <li>Profile Data includes your username and password, purchases or orders made by you, your interests, preferences, feedback and survey responses.</li>
+    <li>Usage Data includes information about how you use our website, products and services.</li>
+    <li>Marketing and Communications Data includes your preferences in receiving marketing from us and our third parties and your communication preferences.</li>
+</ol>
+  </li>
+  <br />
+  We also collect, use and share Aggregated Data such as statistical or demographic data for any purpose.
+  <br /><br />
+  Aggregated Data may be derived from your personal data but is not considered personal data in law as this data does not directly or indirectly reveal your identity.
+  <br /><br />
+  For example, we may aggregate your Usage Data to calculate the percentage of users accessing a specific website feature.
+  <br /><br />
+  However, if we combine or connect Aggregated Data with your personal data so that it can directly or indirectly identify you, we treat the combined data as personal data which will be used in accordance with this privacy notice.
+  <br /><br />
+  We do not collect any Special Categories of Personal Data about you (this includes details about your race or ethnicity, religious or philosophical beliefs, sex life, sexual orientation, political opinions, trade union membership, information about your health and genetic and biometric data). Nor do we collect any information about criminal convictions and offences.
+  <li>
+    <h3>How Do You Use My Personal Data?</h3>
+    Under the GDPR, we must always have a lawful basis for using personal data. This may be because the data is necessary for our performance of a contract with you, because you have consented to our use of your personal data, or because it is in our legitimate business interests to use it. <br /><br />
+
+    Your personal data may be used for the following purposes:
+    <ol type="a">
+      <li>Supplying our services to you. Your personal details are required in order for us to enter into a contract with you.</li>
+      <li>Personalising and tailoring our services for you.</li>
+      <li>Communicating with you. This may include responding to emails or calls from you.</li>
+      <li>Supplying you with information by email that you have opted-in to (you may unsubscribe or opt-out at any time by info@websolutions.services).</li>
+    </ol>
+    <br />
+  </li>
+  With your permission and/or where permitted by law, we may also use your personal data for marketing purposes, which may include contacting you by email with information, news, and offers on our services. You will not be sent any unlawful marketing or spam. We will always work to fully protect your rights and comply with our obligations under the GDPR and the Privacy and Electronic Communications (EC Directive) Regulations 2003, and you will always have the opportunity to opt-out.
+
+  <li>
+    <h3>How Long Will You Keep My Personal Data?</h3>
+    We will not keep your personal data for any longer than is necessary in light of the reason(s) for which it was first collected. Your personal data will therefore be kept for the following periods (or, where there is no fixed period, the following factors will be used to determine how long it is kept):
+    <br /><br />
+    <ol type="a">
+      <li>We will only retain your personal data for as long as necessary to fulfil the purposes we collected it for, including for the purposes of satisfying any legal, accounting, or reporting requirements.</li>
+      <li>To determine the appropriate retention period for personal data, we consider the amount, nature, and sensitivity of the personal data, the potential risk of harm from unauthorized use or disclosure of your personal data, the purposes for which we process your personal data and whether we can achieve those purposes through other means, and the applicable legal requirements.</li>
+      <li>By law we have to keep basic information about our customers (including Contact, Identity, Financial and Transaction Data) for [six] years after they cease being customers for tax purposes.</li>
+      <li>In some circumstances you can ask us to delete your data: see below for further information.</li>
+      <li>In some circumstances we may anonymise your personal data (so that it can no longer be associated with you) for research or statistical purposes in which case we may use this information indefinitely without further notice to you.</li>
+    </ol>
+  </li>
+  <li>
+    <h3>How and Where Do You Store or Transfer My Personal Data?</h3>
+    We will only store or transfer your personal data in the UK. This means that it will be fully protected under the GDPR.
+    <br /><br />
+    The security of your personal data is essential to us, and to protect your data, we take a number of important measures, including the following:<br />
+    <ol type="a">
+      <li>The privacy of your data is guaranteed by us and also the providers of services we use.</li>
+    </ol>
+  </li>
+  <li>
+    <h3>Do You Share My Personal Data?</h3>
+    We may sometimes contract with the following third parties to supply certain services. These may include payment processing, delivery, and marketing. In some cases, those third parties may require access to some or all of your personal data that we hold.
+    <br /><br />
+    <ol type="a">
+      <li>Smart Hosting (Domain & Hosting Company) - https://www.bestwebhosting.co.uk/</li>
+          - Purchase Clients Domain<br />
+          - Hosting Master Reseller Account
+      <li>Instant Print (Printing Company) - https://www.instantprint.co.uk/</li>
+          - To Provide Client Artwork<br />
+          - To Provide Client Delivery Address
+    </ol>
+  </li>
+  If any of your personal data is required by a third party, as described above, we will take steps to ensure that your personal data is handled safely, securely, and in accordance with your rights, our obligations, and the third party’s obligations under the law, as described above in Part 8.
+  <br /><br />
+If any personal data is transferred outside of the EEA, we will take suitable steps in order to ensure that your personal data is treated just as safely and securely as it would be within the UK and under the GDPR, as explained above in Part 8.
+<br /><br />
+In some limited circumstances, we may be legally required to share certain personal data, which might include yours, if we are involved in legal proceedings or complying with legal obligations, a court order, or the instructions of a government authority.
+<li>
+  <h3>Can I Withhold Information?</h3>
+  You may access certain areas of Our Site without providing any personal data at all. However, to use all features and functions available on Our Site you may be required to submit or allow for the collection of certain data.
+</li>
+<li>
+  <h3>How Can I Access My Personal Data?</h3>
+  If you want to know what personal data we have about you, you can ask us for details of that personal data and for a copy of it (where any such personal data is held). This is known as a “subject access request”.
+<br /><br />
+  All subject access requests should be made in writing and sent to the email or postal addresses shown in Part 12.
+There is not normally any charge for a subject access request. If your request is ‘manifestly unfounded or excessive’ (for example, if you make repetitive requests) a fee may be charged to cover our administrative costs in responding.
+<br /><br />
+We will respond to your subject access request within 30 days and, in any case, not more than one month of receiving it. Normally, we aim to provide a complete response, including a copy of your personal data within that time. In some cases, however, particularly if your request is more complex, more time may be required up to a maximum of three months from the date we receive your request. You will be kept fully informed of our progress.
+</li>
+<li>
+  <h3>How Do I Contact You?</h3>
+  To contact us about anything to do with your personal data and data protection, including to make a subject access request, please use the following details (for the attention of Umar Kaleem):<br /><br />
+<b>Email address:</b> GDPR@websolutions.services.<br />
+<b>Telephone number:</b> 0208 064 0276.<br />
+<b>Postal Address:</b> 131 Gordon Road, Ilford, Essex, IG1 2XT.
+</li>
+<li>
+  <h3>Changes to this Privacy Policy</h3>
+  We may change this Privacy Notice from time to time. This may be necessary, for example, if the law changes, or if we change our business in a way that affects personal data protection.<br /><br />
+  Any changes will be immediately posted on Our Site and you will be deemed to have accepted the terms of the Privacy Policy on your first use of Our Site following the alterations. We recommend that you check this page regularly to keep up-to-date.
+</li>
+</ol>
+</div>
+<br /><br /><br />
 
 <?php include "includes/footer.php" ?>
