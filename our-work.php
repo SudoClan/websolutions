76768ca58709f794afa08b8eeabc16b8be4ddeea
Umar <umark.ws@gmail.com>
2018-09-09 13:04:13 +0100

    Updated banner with portfolio

--- our-work.php
+++ our-work.php
@@ -2,6 +2,94 @@
 <?php include "includes/nav.php" ?>
 <?php include "includes/bannerOurWork.php" ?>
 
+<div id="features">
+    <div class="container">
+        <div class="row">
+    <div class="col-md-4 feature--item-h">
+        <!-- Feature Item Start -->
+        <div class="feature--item">
+            <div class="feature--icon">
+                <img src="img/portfolio/namaaste-kitchen.jpg" alt="" class="img-responsive center-block">
+            </div>
+            <div class="feature--content">
+                <h2>Namaaste Kitchen</h2>
+                <p><a href="http://www.namaastekitchen.co.uk/" target="_blank" style="color:black;">http://www.namaastekitchen.co.uk</a></p>
+            </div>
+        </div>
+        <!-- Feature Item End -->
+    </div>
+    <div class="col-md-4 feature--item-h">
+        <!-- Feature Item Start -->
+        <div class="feature--item">
+            <div class="feature--icon">
+                <img src="img/portfolio/woodgrange-pharmacy.jpg" alt="" class="img-responsive center-block">
+            </div>
+            <div class="feature--content">
+                <h2>Woodgrange Pharmacy</h2>
+                <p><a href="https://mywoodgrangepharmacy.com/" target="_blank" style="color:black;">https://mywoodgrangepharmacy.com</a></p>
+            </div>
+        </div>
+        <!-- Feature Item End -->
+    </div>
+    <div class="col-md-4 feature--item-h">
+        <!-- Feature Item Start -->
+        <div class="feature--item">
+            <div class="feature--icon">
+                <img src="img/portfolio/sarahealth.jpg" alt="" class="img-responsive center-block">
+            </div>
+            <div class="feature--content">
+                <h2>Sarah Health</h2>
+                <p><a href="https://sarahealth.net/" target="_blank" style="color:black;">https://sarahealth.net</a></p>
+            </div>
+        </div>
+        <!-- Feature Item End -->
+    </div>
+</div>
+  <hr>
+  <br>
 
+  <div class="row">
+      <div class="col-md-4 feature--item-h">
+          <!-- Feature Item Start -->
+          <div class="feature--item">
+              <div class="feature--icon">
+                  <img src="img/icons/seo-optimised.jpg" alt="" class="img-responsive center-block">
+              </div>
+              <div class="feature--content">
+                  <h2><span>SEO</span> Optimised</h2>
+                  <p>The website will be built with SEO in mind. In doing so all platforms will be laid out to the site in order for the on page optimisation to take place.</p>
+              </div>
+          </div>
+          <!-- Feature Item End -->
+      </div>
+      <div class="col-md-4 feature--item-h">
+          <!-- Feature Item Start -->
+          <div class="feature--item">
+              <div class="feature--icon">
+                  <img src="img/icons/unlimited-revisions.jpg" alt="" class="img-responsive center-block">
+              </div>
+              <div class="feature--content">
+                  <h2><span>UNLIMITED</span> Revisions</h2>
+                  <p>Our homepage design has an unlimited design revision on the first stage of design. This is an ensurity you get exactly what you want.</p>
+              </div>
+          </div>
+          <!-- Feature Item End -->
+      </div>
+      <div class="col-md-4 feature--item-h">
+          <!-- Feature Item Start -->
+          <div class="feature--item">
+              <div class="feature--icon">
+                  <img src="img/icons/dedicated-server.jpg" alt="" class="img-responsive center-block">
+              </div>
+              <div class="feature--content">
+                  <h2><span>DEDICATED</span> Server</h2>
+                  <p>Get a FREE .com or .co.uk of your choice, with it get 1 year FREE hosting alongside FREE emails. With our packages we cover you for the 1st year.</p>
+              </div>
+          </div>
+          <!-- Feature Item End -->
+      </div>
+  </div>
+</div>
+</div>
 
 <?php include "includes/footer.php" ?>
