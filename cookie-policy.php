246a42ddea67ff2ec5099a0e206f3d5cda5ede3b
Umar <umark.ws@gmail.com>
2018-09-09 20:50:25 +0100

    Content updated

--- cookie-policy.php
+++ cookie-policy.php
@@ -2,6 +2,28 @@
 <?php include "includes/nav.php" ?>
 <?php include "includes/bannerCookie-Policy.php" ?>
 
+<br /><br /><br />
+<div class="container">
+Websolutions is strongly committed to protecting the privacy of users of this website.
+We collect data on this website via:
 
+<h3>Enquiry Forms</h3>
+We use a custom coded PHP contact form to power any forms seen on our site. Data captured through the Contact form on our website and will be used to contact you in order to respond to your enquiry. A backup of your data will also be stored in email to our website. Only authorised individuals will be able to access your data, and it will not be disclosed to third parties. If you would like your data to be removed in advance, please email cookies@websolutions.services.
+
+<h3>Cookies</h3>
+Cookies are small data files that are placed on your computer as you browse our website. Most websites do this too.
+<br /><br />
+They improve things by:
+<br /><br />
+<ul>
+  <li>Remembering settings, so you don’t have to keep re-entering them whenever you visit a new page.</li>
+  <li>Remembering information you’ve given (e.g. your postcode) so you don’t need to keep entering it.</li>
+  <li>Measuring how you use the website so we can make sure it meets your needs.</li>
+  <li>Our cookies aren’t used to identify you personally. They’re just here to make the site work better for you. Indeed, you can manage and/or delete these small files as you wish. To learn more about cookies and how to manage them, visit AboutCookies.org.</li>
+</ul>
+
+If you have any questions or concerns about this, please email us at: cookies@websolutions.services.
+</div>
+<br /><br /><br />
 
 <?php include "includes/footer.php" ?>
