1e93576d692200b9d8e252484022040ab001279a
Umar <umark.ws@gmail.com>
2018-09-24 11:30:13 +0100

    Package edits

--- web-maintenance.php
+++ web-maintenance.php
@@ -1,131 +1,84 @@
 <?php include "includes/header.php" ?>
 <?php include "includes/nav.php" ?>
 <?php include "includes/bannerWebMaintenance.php" ?>
-<!-- Features Area Start -->
-        <div id="features">
-            <div class="container">
 
-                <div class="row">
-                    <div class="col-md-4 feature--item-h">
-                        <!-- Feature Item Start -->
-                        <div class="feature--item">
-                            <div class="feature--icon">
-                                <img src="img/icons/cms.jpg" alt="" class="img-responsive center-block">
-                            </div>
-                            <div class="feature--content">
-                                <h2><span>CMS</span> System</h2>
-                                <p>We develop using the latest content management and give the client the option to choose from wither WordPress, Joomla or Drupal. Edit your website with delight!</p>
-                            </div>
-                        </div>
-                        <!-- Feature Item End -->
-                    </div>
-                    <div class="col-md-4 feature--item-h">
-                        <!-- Feature Item Start -->
-                        <div class="feature--item">
-                            <div class="feature--icon">
-                                <img src="img/icons/bespoke-website.jpg" alt="" class="img-responsive center-block">
-                            </div>
-                            <div class="feature--content">
-                                <h2><span>BESPOKE</span> Websites</h2>
-                                <p>All our websites are bespoke and use a no template policy. Our objective always is to design, develop and then execute.</p>
-                            </div>
-                        </div>
-                        <!-- Feature Item End -->
-                    </div>
-                    <div class="col-md-4 feature--item-h">
-                        <!-- Feature Item Start -->
-                        <div class="feature--item">
-                            <div class="feature--icon">
-                                <img src="img/icons/logo-design.jpg" alt="" class="img-responsive center-block">
-                            </div>
-                            <div class="feature--content">
-                                <h2><span>LOGO</span> Design</h2>
-                                <p>We design you a unique logo of your choice with the given package. We can achieve any look of your choice, or better that give us creative control.</p>
-                            </div>
-                        </div>
-                        <!-- Feature Item End -->
-                    </div>
-                </div>
-                  <hr>
-                  <br>
+<div id="pricing" class="bg--whitesmoke">
+  <div class="container">
+  <div class="section--title"> <div class="row"> <div class="col-md-6"> <h2>Choose your <span>Plan</span></h2> </div><div class="col-md-6"> <p>Get the plan that suits your business.<br />Plan that will grow your business.</p>
+  </div></div></div>
+    <!--<div class="row reset-gutter">-->
 
-                  <div class="row">
-                      <div class="col-md-4 feature--item-h">
-                          <!-- Feature Item Start -->
-                          <div class="feature--item">
-                              <div class="feature--icon">
-                                  <img src="img/icons/seo-optimised.jpg" alt="" class="img-responsive center-block">
-                              </div>
-                              <div class="feature--content">
-                                  <h2><span>SEO</span> Optimised</h2>
-                                  <p>The website will be built with SEO in mind. In doing so all platforms will be laid out to the site in order for the on page optimisation to take place.</p>
-                              </div>
-                          </div>
-                          <!-- Feature Item End -->
-                      </div>
-                      <div class="col-md-4 feature--item-h">
-                          <!-- Feature Item Start -->
-                          <div class="feature--item">
-                              <div class="feature--icon">
-                                  <img src="img/icons/unlimited-revisions.jpg" alt="" class="img-responsive center-block">
-                              </div>
-                              <div class="feature--content">
-                                  <h2><span>UNLIMITED</span> Revisions</h2>
-                                  <p>Our homepage design has an unlimited design revision on the first stage of design. This is an ensurity you get exactly what you want.</p>
-                              </div>
-                          </div>
-                          <!-- Feature Item End -->
-                      </div>
-                      <div class="col-md-4 feature--item-h">
-                          <!-- Feature Item Start -->
-                          <div class="feature--item">
-                              <div class="feature--icon">
-                                  <img src="img/icons/dedicated-server.jpg" alt="" class="img-responsive center-block">
-                              </div>
-                              <div class="feature--content">
-                                  <h2><span>DEDICATED</span> Server</h2>
-                                  <p>Get a FREE .com or .co.uk of your choice, with it get 1 year FREE hosting alongside FREE emails. With our packages we cover you for the 1st year.</p>
-                              </div>
-                          </div>
-                          <!-- Feature Item End -->
-                      </div>
-                  </div>
-
-                  <hr>
-                  <br>
-                <div class="row">
-                    <!-- Pricing 2 Item Start -->
-                    <div  class="col-md-4 pricing-2--item">
-                        <div class="pricing-2--header">
-                            <h2><b>Launch Package</b></h2>
-                            <h4>Perfect for Launching your message out there</h4>
-                        </div>
-                    </div>
-                    <!-- Pricing 2 Item End -->
-                    <!-- Pricing 2 Item Start -->
-                    <div class="col-md-4 pricing-2--item">
-                        <div class="pricing-2--header">
-                            <h2><b>Booster Package</b></h2>
-                            <h4>Perfect for expanding your business or product</h4>
-                        </div>
-                    </div>
-                    <!-- Pricing 2 Item End -->
-                    <!-- Pricing 2 Item Start -->
-                    <div class="col-md-4 pricing-2--item">
-                        <div class="pricing-2--header">
-                            <h2><b>Orbit Package</b></h2>
-                            <h4>Whatever your Bespoke requirements are, we will help</h4>
-                        </div>
-                    </div>
-                    <!-- Pricing 2 Item End -->
-                    <div style="margin:auto; text-align:center; position:relative; top:40px;">
-                      <a href="web-hosting.php" class="btn--primary btn--ripple">Find out More about our Packages</a>
-                    </div>
-                </div>
+      <div class="col-md-4 pricing--item-h"> <div class="pricing--item">
+        <div class="pricing--body">
+          <div class="pricing--details">
+            <h2><strong>Basic</strong> Package</h2>
+            <h3>£20.00/mo</h3>
+            <p>Launching your product<br />ready for take off</p>
+            <hr>
+              <div class="banner--content pricing-table">
+              <ul>
+                <li class="pricing-table-p">5GB SSD Disk Space</li>
+                <li class="pricing-table-p">Unlimited Bandwidth</li>
+                <li class="pricing-table-p">5 Database & Emails</li>
+                <li class="pricing-table-p">Free DDoS Protection</li>
+                <li class="pricing-table-p">Free Website Builder</li>
+                <li class="pricing-table-p">Monthly Backup</li>
+                <li class="pricing-table-p">SSL Certificate (Optional)</li>
+              </ul>
             </div>
-        </div>
+          </div></div>
+      <div class="pricing--footer">
+        <a href="tel:02080640276" class="btn--primary btn--ripple">Call us to Order</a>
+      </div></div></div>
 
-        <!-- Features Area End -->
+      <div class="col-md-4 pricing--item-h"> <div class="pricing--item">
+        <div class="pricing--body">
+          <div class="pricing--details">
+            <h2><strong>Recommended</strong> Package</h2>
+            <h3>£50.00/mo</h3>
+            <p>Perfect for expanding your<br />business or product</p>
+            <hr>
+              <div class="banner--content pricing-table">
+              <ul>
+                <li class="pricing-table-p">500GB SSD Disk Space</li>
+                <li class="pricing-table-p">Unlimited Bandwidth</li>
+                <li class="pricing-table-p">Unlimited Database & Emails</li>
+                <li class="pricing-table-p">Free DDoS Protection</li>
+                <li class="pricing-table-p">Optimised for Apps and CMS</li>
+                <li class="pricing-table-p">Weekly Backup</li>
+                <li class="pricing-table-p">SSL Certificate (Optional)</li>
+              </ul>
+            </div>
+          </div></div>
+      <div class="pricing--footer">
+      <a href="tel:02080640276" class="btn--primary btn--ripple">Call us to Order</a>
+      </div></div></div>
+
+      <div class="col-md-4 pricing--item-h"> <div class="pricing--item">
+        <div class="pricing--body">
+          <div class="pricing--details">
+            <h2><strong>Advanced</strong> Package</h2>
+            <h3>£100.00/mo</h3>
+            <p>Advanced spaceship that<br />circle the earth</p>
+            <hr>
+              <div class="banner--content pricing-table">
+              <ul>
+                <li class="pricing-table-p">Unlimited SSD Disk Space</li>
+                <li class="pricing-table-p">Unlimited Bandwidth</li>
+                <li class="pricing-table-p">Unlimited Database & Emails</li>
+                <li class="pricing-table-p">Free DDoS Protection</li>
+                <li class="pricing-table-p">SpamExperts Protection</li>
+                <li class="pricing-table-p">Daily Backup</li>
+                <li class="pricing-table-p">Free 256 Bit SSL Certificate</li>
+              </ul>
+            </div>
+          </div></div>
+      <div class="pricing--footer">
+      <a href="tel:02080640276" class="btn--primary btn--ripple">Call us to Order</a>
+      </div></div></div>
+    <!--</div>-->
+  </div>
+</div>
 
 <?php include "includes/reviews.php" ?>
 <?php include "includes/footer.php" ?>
