a85e48ad90c3538a8019f656bb2ca2aad36100a6
Umar <umark.ws@gmail.com>
2018-09-25 15:03:10 +0100

    Title Updated

--- our-work.php
+++ our-work.php
@@ -1,3 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<title>Our Portfolio</title>
+
 <?php include "includes/header.php" ?>
 <?php include "includes/nav.php" ?>
 <?php include "includes/bannerOurWork.php" ?>
