49ad6425d5917663e2a99b443270f77d0b6aa3d8
Umar <umark.ws@gmail.com>
2018-09-25 15:03:41 +0100

    Title Updated

--- contact.php
+++ contact.php
@@ -1,3 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<title>Contact Us</title>
+
 <?php include "includes/header.php" ?>
 <?php include "includes/nav.php" ?>
 <?php include "includes/bannerContact.php" ?>
