7f97ec116d38818a61878ad729942dfbcd5cc1a9
Umar <umark.ws@gmail.com>
2018-09-25 14:51:41 +0100

    Homepage Title

--- includes/header.php
+++ includes/header.php
@@ -1,14 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
+
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 
     <meta name="description" content="">
     <meta name="author" content="">
-
-    <title>Cloud Hosting, Web Development, All in One, Web Solutions in London</title>
 
     <!-- ====Favicons==== -->
     <!--<link rel="shortcut icon" href="favicon.png" type="image/x-icon">
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<title>Cloud Hosting, Web Development, All in One, Web Solutions in London</title>
+
 <?php include "includes/header.php" ?>
 <?php include "includes/nav.php" ?>
 <?php include "includes/bannerHome.php" ?>
